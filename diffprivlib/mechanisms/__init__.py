82629d4206d699b97b171be53b4e551e57fb6507
Naoise Holohan <naoise@ibm.com>
2019-02-15 15:33:09 +0000

    Adding Gaussian to init file

--- diffprivlib/mechanisms/__init__.py
+++ diffprivlib/mechanisms/__init__.py
@@ -2,6 +2,7 @@ from .utils import DPMechanism, TruncationAndFoldingMachine
 
 from .binary import Binary
 from .exponential import Exponential, ExponentialHierarchical
+from .gaussian import Gaussian
 from .geometric import Geometric, GeometricFolded, GeometricTruncated
 from .laplace import Laplace, LaplaceBoundedDomain, LaplaceBoundedNoise, LaplaceFolded, LaplaceTruncated
 from .staircase import Staircase
