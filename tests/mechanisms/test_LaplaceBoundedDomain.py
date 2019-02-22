72b920e11bdcd1e057ce8e55d700a5edbd979ba5
Naoise Holohan <naoise@ibm.com>
2019-02-22 11:23:46 +0000

    Adding test for Laplace Bounded Domain

---
+++ tests/mechanisms/test_LaplaceBoundedDomain.py
@@ -0,0 +1,81 @@
+import numpy as np
+from unittest import TestCase
+
+from diffprivlib.mechanisms import LaplaceBoundedDomain, DPMechanism
+
+mech = LaplaceBoundedDomain()
+
+
+class TestLaplaceBoundedDomain(TestCase):
+    def test_not_none(self):
+        self.assertIsNotNone(mech)
+        _mech = mech.copy()
+        self.assertIsNotNone(_mech)
+
+    def test_class(self):
+        self.assertTrue(issubclass(LaplaceBoundedDomain, DPMechanism))
+
+    def test_no_params(self):
+        _mech = mech.copy()
+        with self.assertRaises(ValueError):
+            _mech.randomise(1)
+
+    def test_no_sensitivity(self):
+        _mech = mech.copy().set_epsilon(1).set_bounds(0, 1)
+        with self.assertRaises(ValueError):
+            _mech.randomise(1)
+
+    def test_no_epsilon(self):
+        _mech = mech.copy().set_sensitivity(1).set_bounds(0, 1)
+        with self.assertRaises(ValueError):
+            _mech.randomise(1)
+
+    def test_no_bounds(self):
+        _mech = mech.copy().set_sensitivity(1).set_epsilon(1)
+        with self.assertRaises(ValueError):
+            _mech.randomise(1)
+
+    def test_non_numeric(self):
+        _mech = mech.copy().set_sensitivity(1).set_epsilon(1).set_bounds(0, 1)
+        with self.assertRaises(TypeError):
+            _mech.randomise("Hello")
+
+    def test_zero_median(self):
+        _mech = mech.copy().set_sensitivity(1).set_epsilon(1).set_bounds(0, 1)
+        vals = []
+
+        for i in range(10000):
+            vals.append(_mech.randomise(0.5))
+
+        median = float(np.median(vals))
+        self.assertAlmostEqual(np.abs(median), 0.5, delta=0.1)
+
+    def test_neighbors(self):
+        epsilon = 1
+        runs = 10000
+        _mech = mech.copy().set_sensitivity(1).set_epsilon(1).set_bounds(0, 1)
+        count = [0, 0]
+
+        for i in range(runs):
+            val0 = _mech.randomise(0)
+            if val0 <= 0.5:
+                count[0] += 1
+
+            val1 = _mech.randomise(1)
+            if val1 <= 0.5:
+                count[1] += 1
+
+        self.assertGreater(count[0], count[1])
+        self.assertLessEqual(count[0] / runs, np.exp(epsilon) * count[1] / runs + 0.1)
+
+    def test_within_bounds(self):
+        _mech = mech.copy().set_sensitivity(1).set_epsilon(1).set_bounds(0, 1)
+        vals = []
+
+        for i in range(1000):
+            vals.append(_mech.randomise(0.5))
+
+        vals = np.array(vals)
+
+        self.assertTrue(np.all(vals >= 0))
+        self.assertTrue(np.all(vals <= 1))
