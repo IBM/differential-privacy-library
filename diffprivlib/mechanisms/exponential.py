Fixes error with exponential, incorrect probability calculation

diffprivlib/mechanisms/exponential.py
@@ -20,6 +20,7 @@ class Exponential(DPMechanism):
         self._utility_values = None
         self._normalising_constant = None
         self._sensitivity = None
+        self._balanced_tree = False
 
     def __repr__(self):
         output = super().__repr__()
@@ -114,7 +115,7 @@ class Exponential(DPMechanism):
         return utility_list
 
     def _build_normalising_constant(self, re_eval=False):
-        balanced_hierarchy = True
+        balanced_tree = True
         first_constant_value = None
         normalising_constant = {}
 
@@ -128,11 +129,12 @@ class Exponential(DPMechanism):
 
             if first_constant_value is None:
                 first_constant_value = constant_value
-            elif constant_value != first_constant_value:
-                balanced_hierarchy = False
+            elif not np.isclose(constant_value, first_constant_value):
+                balanced_tree = False
 
-        if balanced_hierarchy and not re_eval:
-            self._sensitivity /= 2
+        # If the tree is balanced, we can eliminate the doubling factor
+        if balanced_tree and not re_eval:
+            self._balanced_tree = True
             return self._build_normalising_constant(True)
 
         return normalising_constant
@@ -147,7 +149,8 @@ class Exponential(DPMechanism):
         return self._utility_values[value1 + "::" + value2]
 
     def _get_prob(self, value1, value2):
-        return np.exp(- self._epsilon * self._get_utility(value1, value2) / self._sensitivity)
+        balancing_factor = 1 if self._balanced_tree else 2
+        return np.exp(- self._epsilon * self._get_utility(value1, value2) / balancing_factor / self._sensitivity)
 
     def check_inputs(self, value):
         """

tests/mechanisms/test_Exponential.py
@@ -123,5 +123,5 @@ class TestExponential(TestCase):
                 count[2] += 1
 
         # print("A: %d, B: %d, C: %d" % (count[0], count[1], count[2]))
-        self.assertAlmostEqual(count[0] / runs, np.exp(epsilon) * count[2] / runs, delta=0.1)
+        self.assertLessEqual(count[0] / runs, np.exp(epsilon) * count[2] / runs + 0.05)
         self.assertAlmostEqual(count[0] / count[1], count[1] / count[2], delta=0.1)

tests/mechanisms/test_ExponentialHierarchical.py
@@ -75,6 +75,7 @@ class TestExponentialHierarchical(TestCase):
     def test_distrib(self):
         epsilon = np.log(2)
         runs = 20000
+        balanced_tree = False
         _mech = mech.copy().set_epsilon(epsilon).set_hierarchy([["A", "B"], ["C"]])
         count = [0, 0, 0]
 
@@ -87,6 +88,57 @@ class TestExponentialHierarchical(TestCase):
             elif val == "C":
                 count[2] += 1
 
+        # print(_mech.get_utility_list())
+        # print(_mech._sensitivity)
+
         # print("A: %d, B: %d, C: %d" % (count[0], count[1], count[2]))
-        self.assertAlmostEqual(count[0] / runs, np.exp(epsilon) * count[2] / runs, delta=0.1)
+        self.assertAlmostEqual(count[0] / runs, np.exp(epsilon / (1 if balanced_tree else 2)) * count[2] / runs, delta=0.1)
         self.assertAlmostEqual(count[0] / count[1], count[1] / count[2], delta=0.1)
+
+    def test_neighbours(self):
+        epsilon = np.log(2)
+        runs = 20000
+        _mech = mech.copy().set_epsilon(epsilon).set_hierarchy([["A", "B"], ["C"]])
+        count = [0, 0, 0]
+
+        for i in range(runs):
+            val = _mech.randomise("A")
+            if val == "A":
+                count[0] += 1
+
+            val = _mech.randomise("B")
+            if val == "A":
+                count[1] += 1
+
+            val = _mech.randomise("C")
+            if val == "A":
+                count[2] += 1
+
+        # print("Output: A\nInput: A: %d, B: %d, C: %d" % (count[0], count[1], count[2]))
+        self.assertLessEqual(count[0] / runs, np.exp(epsilon) * count[1] / runs)
+        self.assertLessEqual(count[0] / runs, np.exp(epsilon) * count[2] / runs)
+        self.assertLessEqual(count[1] / runs, np.exp(epsilon) * count[2] / runs)
+
+    def test_neighbours_flat_hierarchy(self):
+        epsilon = np.log(2)
+        runs = 20000
+        _mech = mech.copy().set_epsilon(epsilon).set_hierarchy(["A", "B", "C"])
+        count = [0, 0, 0]
+
+        for i in range(runs):
+            val = _mech.randomise("A")
+            if val == "A":
+                count[0] += 1
+
+            val = _mech.randomise("B")
+            if val == "A":
+                count[1] += 1
+
+            val = _mech.randomise("C")
+            if val == "A":
+                count[2] += 1
+
+        # print("(Output: A) Input: A: %d, B: %d, C: %d" % (count[0], count[1], count[2]))
+        self.assertLessEqual(count[0] / runs, np.exp(epsilon) * count[1] / runs + 0.05)
+        self.assertLessEqual(count[0] / runs, np.exp(epsilon) * count[2] / runs + 0.05)
+        self.assertLessEqual(count[1] / runs, np.exp(epsilon) * count[2] / runs + 0.05)