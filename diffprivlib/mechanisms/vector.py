Adds gradient to LogisticRegression and fixes regularisation (closes #22)

diffprivlib/mechanisms/vector.py
@@ -20,8 +20,7 @@ class Vector(DPMechanism):
         self._function_sensitivity = None
         self._data_sensitivity = 1
         self._d = None
-        self._n = None
-        self._lambda = 0.01
+        self._alpha = 0.01
 
     def set_epsilon_delta(self, epsilon, delta):
         """
@@ -58,21 +57,21 @@ class Vector(DPMechanism):
         self._data_sensitivity = data_sensitivity
         return self
 
-    def set_lambda(self, lam):
+    def set_alpha(self, alpha):
         """
-        Set the regularisation parameter lambda for the mechanism.
+        Set the regularisation parameter alpha for the mechanism.
 
-        :param lam: Regularisation parameter, default is 0.01
-        :type lam: `float`
+        :param alpha: Regularisation parameter, default is 0.01
+        :type alpha: `float`
         :return: self
         """
-        if not isinstance(lam, Real):
-            raise TypeError("Lambda must be numeric")
+        if not isinstance(alpha, Real):
+            raise TypeError("Alpha must be numeric")
 
-        if lam <= 0:
-            raise ValueError("Lambda must be strictly positive")
+        if alpha <= 0:
+            raise ValueError("Alpha must be strictly positive")
 
-        self._lambda = lam
+        self._alpha = alpha
         return self
 
     def check_inputs(self, value):
@@ -93,29 +92,23 @@ class Vector(DPMechanism):
         if self._data_sensitivity is None or self._function_sensitivity is None:
             raise ValueError("Sensitivities must be set")
 
-        if self._n is None or self._d is None:
-            raise ValueError("Dimensions n and d must be set")
+        if self._d is None:
+            raise ValueError("Dimension d must be set")
 
         return True
 
-    def set_dimensions(self, d, n):
+    def set_dimension(self, d):
         """
-        Set the dimensions of the function output `d` and the number of datapoints `n`.
+        Set the dimension of the function output `d`.
 
         :param d: Function output dimension.
         :type d: `int`
-        :param n: Number of datapoints in the dataset.
-        :type n: `int`
         :return: self
         """
 
         if not isinstance(d, Real) or not d >= 1 or not np.isclose(d, int(d)):
             raise ValueError("d must be a strictly positive integer")
 
-        if not isinstance(n, Real) or not n >= 1 or not np.isclose(n, int(n)):
-            raise ValueError("n must be a strictly positive integer")
-
-        self._n = n
         self._d = d
         return self
 
@@ -132,11 +125,13 @@ class Vector(DPMechanism):
 
         c = self._function_sensitivity
         g = self._data_sensitivity
-        epsilon_p = self._epsilon - 2 * np.log(1 + c * g / (self._lambda * self._n))
+        a = self._alpha
+
+        epsilon_p = self._epsilon - 2 * np.log(1 + c * g / (0.5 * a))
         delta = 0
 
         if epsilon_p <= 0:
-            delta = c * g / (self._n * (np.exp(self._epsilon / 4) - 1)) - self._lambda
+            delta = c * g / (np.exp(self._epsilon / 4) - 1) - 0.5 * a
             epsilon_p = self._epsilon / 2
 
         scale = epsilon_p / 2 / g
@@ -150,9 +145,21 @@ class Vector(DPMechanism):
         def output_func(*args):
             w = args[0]
 
-            val = value(*args)
-            val += np.dot(normed_noisy_vector, w) / self._n
-            val += 0.5 * delta * np.dot(w, w)
-            return val
+            func = value(*args)
+
+            if isinstance(func, tuple):
+                func, grad = func
+            else:
+                grad = None
+
+            func += np.dot(normed_noisy_vector, w)
+            func += 0.5 * delta * np.dot(w, w)
+
+            if grad is not None:
+                grad += normed_noisy_vector + delta * w
+
+                return func, grad
+
+            return func
 
         return output_func

diffprivlib/models/logistic_regression.py
@@ -6,11 +6,12 @@ import numpy as np
 from scipy import optimize
 from sklearn.exceptions import ConvergenceWarning
 from sklearn import linear_model
-from sklearn.linear_model.logistic import _check_solver, _check_multi_class, _logistic_loss
+from sklearn.linear_model.logistic import _check_solver, _check_multi_class, _logistic_loss_and_grad
 from sklearn.utils import check_X_y, check_array, check_consistent_length
 from sklearn.utils.multiclass import check_classification_targets
 
 from diffprivlib.mechanisms import Vector
+from diffprivlib.utils import PrivacyLeakWarning
 
 
 class LogisticRegression(linear_model.LogisticRegression):
@@ -61,12 +62,11 @@ class LogisticRegression(linear_model.LogisticRegression):
             self.multi_class = "ovr"
 
         max_norm = np.linalg.norm(X, axis=1).max()
-        if max_norm > 1:
-            warnings.warn("Differential privacy is only guaranteed for data whose rows have a 2-norm of at most 1. "
+        if max_norm > self.data_norm:
+            warnings.warn("Differential privacy is only guaranteed for data whose rows have a 2-norm of at most %g. "
                           "Got %f\n"
                           "Translate and/or scale the data accordingly to ensure differential privacy is achieved."
-                          % max_norm,
-                          RuntimeWarning)
+                          % (self.data_norm, max_norm), PrivacyLeakWarning)
 
         solver = _check_solver(self.solver, self.penalty, self.dual)
         self.max_iter = int(self.max_iter)
@@ -312,6 +312,10 @@ def logistic_regression_path(X, y, epsilon=1.0, data_norm=1.0, pos_class=None, C
 
     solver = _check_solver(solver, penalty, dual)
 
+    # Data norm increases if intercept is included
+    if fit_intercept:
+        data_norm = np.sqrt(data_norm ** 2 + 1)
+
     # Pre-processing.
     if check_input:
         X = check_array(X, accept_sparse='csr', dtype=np.float64, accept_large_sparse=solver != 'liblinear')
@@ -349,14 +353,17 @@ def logistic_regression_path(X, y, epsilon=1.0, data_norm=1.0, pos_class=None, C
     coefs = list()
     n_iter = np.zeros(len(Cs), dtype=np.int32)
     for i, C in enumerate(Cs):
-        vector_mech = Vector().set_dimensions(n_features + int(fit_intercept), n_samples).set_epsilon(epsilon) \
-            .set_lambda(1. / C).set_sensitivity(0.25, data_norm)
-        noisy_logistic_loss = vector_mech.randomise(_logistic_loss)
+        vector_mech = Vector()\
+            .set_dimension(n_features + int(fit_intercept))\
+            .set_epsilon(epsilon)\
+            .set_alpha(1. / C)\
+            .set_sensitivity(0.25, data_norm)
+        noisy_logistic_loss = vector_mech.randomise(_logistic_loss_and_grad)
 
         iprint = [-1, 50, 1, 100, 101][np.searchsorted(np.array([0, 1, 2, 3]), verbose)]
         w0, loss, info = optimize.fmin_l_bfgs_b(noisy_logistic_loss, w0, fprime=None,
                                                 args=(X, target, 1. / C, sample_weight), iprint=iprint, pgtol=tol,
-                                                maxiter=max_iter, approx_grad=True)
+                                                maxiter=max_iter)
         if info["warnflag"] == 1:
             warnings.warn("lbfgs failed to converge. Increase the number of iterations.", ConvergenceWarning)
 

tests/mechanisms/test_Vector.py
@@ -27,17 +27,17 @@ class TestVector(TestCase):
             self.mech.randomise(self.func)
 
     def test_no_epsilon(self):
-        self.mech.set_dimensions(3, 10).set_sensitivity(1)
+        self.mech.set_dimension(3).set_sensitivity(1)
         with self.assertRaises(ValueError):
             self.mech.randomise(self.func)
 
     def test_neg_epsilon(self):
-        self.mech.set_dimensions(3, 10).set_sensitivity(1)
+        self.mech.set_dimension(3).set_sensitivity(1)
         with self.assertRaises(ValueError):
             self.mech.set_epsilon(-1)
 
     def test_inf_epsilon(self):
-        self.mech.set_dimensions(3, 10).set_sensitivity(1).set_epsilon(float("inf"))
+        self.mech.set_dimension(3).set_sensitivity(1).set_epsilon(float("inf"))
 
         for i in range(100):
             noisy_func = self.mech.randomise(self.func)
@@ -45,24 +45,24 @@ class TestVector(TestCase):
             self.assertAlmostEqual(noisy_func(np.ones(3)), 3)
 
     def test_no_sensitivity(self):
-        self.mech.set_dimensions(3, 10).set_epsilon(1)
+        self.mech.set_dimension(3).set_epsilon(1)
         with self.assertRaises(ValueError):
             self.mech.randomise(self.func)
 
     def test_numeric_input(self):
-        self.mech.set_dimensions(3, 10).set_epsilon(1).set_sensitivity(1)
+        self.mech.set_dimension(3).set_epsilon(1).set_sensitivity(1)
 
         with self.assertRaises(TypeError):
             self.mech.randomise(1)
 
     def test_string_input(self):
-        self.mech.set_dimensions(3, 10).set_epsilon(1).set_sensitivity(1)
+        self.mech.set_dimension(3).set_epsilon(1).set_sensitivity(1)
 
         with self.assertRaises(TypeError):
             self.mech.randomise("1")
 
     def test_sets_once(self):
-        self.mech.set_dimensions(3, 10).set_epsilon(1).set_sensitivity(1)
+        self.mech.set_dimension(3).set_epsilon(1).set_sensitivity(1)
         noisy_func = self.mech.randomise(self.func)
         answer = noisy_func(np.ones(3))
 
@@ -70,7 +70,7 @@ class TestVector(TestCase):
             self.assertEqual(noisy_func(np.ones(3)), answer)
 
     def test_different_result(self):
-        self.mech.set_dimensions(3, 10).set_epsilon(1).set_sensitivity(1)
+        self.mech.set_dimension(3).set_epsilon(1).set_sensitivity(1)
         noisy_func = self.mech.randomise(self.func)
 
         for i in range(10):

tests/models/test_LogisticRegression.py
@@ -2,7 +2,7 @@ import numpy as np
 from unittest import TestCase
 
 from diffprivlib.models.logistic_regression import LogisticRegression
-from diffprivlib.utils import global_seed
+from diffprivlib.utils import global_seed, PrivacyLeakWarning
 
 
 class TestLogisticRegression(TestCase):
@@ -21,7 +21,7 @@ class TestLogisticRegression(TestCase):
 
         clf = LogisticRegression()
 
-        with self.assertWarns(RuntimeWarning):
+        with self.assertWarns(PrivacyLeakWarning):
             clf.fit(X, y)
 
     def test_trinomial(self):
@@ -44,7 +44,7 @@ class TestLogisticRegression(TestCase):
         X -= 3.0
         X /= 2.5
 
-        clf = LogisticRegression()
+        clf = LogisticRegression(epsilon=2)
         clf.fit(X, y)
 
         # print(clf.predict(np.array([0.5, 2, 5.5])))