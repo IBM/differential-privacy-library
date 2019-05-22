1fe27a54c1b36fe0c55061213b912ab1be96c246
Naoise Holohan <naoise@ibm.com>
2019-05-22 11:56:27 +0100

    Fixes line too long

--- diffprivlib/mechanisms/__init__.py
+++ diffprivlib/mechanisms/__init__.py
@@ -7,7 +7,8 @@ from diffprivlib.mechanisms.binary import Binary
 from diffprivlib.mechanisms.exponential import Exponential, ExponentialHierarchical
 from diffprivlib.mechanisms.gaussian import Gaussian
 from diffprivlib.mechanisms.geometric import Geometric, GeometricFolded, GeometricTruncated
-from diffprivlib.mechanisms.laplace import Laplace, LaplaceBoundedDomain, LaplaceBoundedNoise, LaplaceFolded, LaplaceTruncated
+from diffprivlib.mechanisms.laplace import Laplace, LaplaceBoundedDomain, LaplaceBoundedNoise, LaplaceFolded,\
+    LaplaceTruncated
 from diffprivlib.mechanisms.staircase import Staircase
 from diffprivlib.mechanisms.uniform import Uniform
 from diffprivlib.mechanisms.vector import Vector
