045701c0131deb081d39040d718d898531dc1f76
Naoise Holohan <naoise@nholohan.ie>
2019-11-13 14:53:22 +0000

    Adding non-private PCA, with some tests

--- diffprivlib/models/__init__.py
+++ diffprivlib/models/__init__.py
@@ -21,3 +21,4 @@ Machine learning models with differential privacy
 from diffprivlib.models.naive_bayes import GaussianNB
 from diffprivlib.models.k_means import KMeans
 from diffprivlib.models.logistic_regression import LogisticRegression
+from diffprivlib.models.pca import PCA
