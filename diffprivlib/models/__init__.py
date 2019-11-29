8300193458a91c0c06a84b9388a693ce7da80d28
Naoise Holohan <naoise@nholohan.ie>
2019-11-29 17:55:32 +0000

    Adding differentially private Linear regression, with tests

--- diffprivlib/models/__init__.py
+++ diffprivlib/models/__init__.py
@@ -20,5 +20,6 @@ Machine learning models with differential privacy
 """
 from diffprivlib.models.naive_bayes import GaussianNB
 from diffprivlib.models.k_means import KMeans
+from diffprivlib.models.linear_regression import LinearRegression
 from diffprivlib.models.logistic_regression import LogisticRegression
 from diffprivlib.models.pca import PCA
