1f53f41c1ef7993aa69568a76e9a4939327cefa2
Naoise Holohan <naoise@nholohan.ie>
2019-12-06 16:02:25 +0000

    Adding PCA documentation

--- diffprivlib/models/pca.py
+++ diffprivlib/models/pca.py
@@ -59,12 +59,114 @@ from diffprivlib.utils import warn_unused_args, copy_docstring, PrivacyLeakWarni
 
 # noinspection PyPep8Naming
 class PCA(sk_pca.PCA):
-    """PCA model, documentation to follow.
+    """Principal component analysis (PCA) with differential privacy.
+
+    This class is a child of :obj:`sklearn.decomposition.PCA`, with amendments to allow for the implementation of
+    differential privacy as given in [IS16]_.  Some parameters of `Scikit Learn`'s model have therefore had to be fixed,
+    including:
+
+        - The only permitted `svd_solver` is 'full'.  Specifying the ``svd_solver`` option will result in a warning;
+        - The parameters ``tol`` and ``iterated_power`` are not applicable (as a consequence of fixing ``svd_solved =
+        'full'``.
+
+    Parameters
+    ----------
+    n_components : int, float, None or str
+        Number of components to keep.
+        If n_components is not set all components are kept::
+
+            n_components == min(n_samples, n_features)
+
+        If ``n_components == 'mle'``, Minka's MLE is used to guess the dimension.
+
+        If ``0 < n_components < 1``, select the number of components such that the amount of variance that needs to be
+        explained is greater than the percentage specified by n_components.
+
+        Hence, the None case results in::
+
+            n_components == min(n_samples, n_features) - 1
+
+    copy : bool, default=True
+        If False, data passed to fit are overwritten and running fit(X).transform(X) will not yield the expected
+        results, use fit_transform(X) instead.
+
+    whiten : bool, optional (default False)
+        When True (False by default) the `components_` vectors are multiplied by the square root of n_samples and
+        then divided by the singular values to ensure uncorrelated outputs with unit component-wise variances.
+
+        Whitening will remove some information from the transformed signal (the relative variance scales of the
+        components) but can sometime improve the predictive accuracy of the downstream estimators by making their
+        data respect some hard-wired assumptions.
+
+    random_state : int, RandomState instance or None, optional (default None)
+        If int, random_state is the seed used by the random number generator; If RandomState instance, random_state
+        is the random number generator.
+
+        .. versionadded:: 0.18.0
+
+    Attributes
+    ----------
+    components_ : array, shape (n_components, n_features)
+        Principal axes in feature space, representing the directions of maximum variance in the data. The components
+        are sorted by ``explained_variance_``.
+
+    explained_variance_ : array, shape (n_components,)
+        The amount of variance explained by each of the selected components.
+
+        Equal to n_components largest eigenvalues of the covariance matrix of X.
+
+        .. versionadded:: 0.18
+
+    explained_variance_ratio_ : array, shape (n_components,)
+        Percentage of variance explained by each of the selected components.
+
+        If ``n_components`` is not set then all components are stored and the sum of the ratios is equal to 1.0.
+
+    singular_values_ : array, shape (n_components,)
+        The singular values corresponding to each of the selected components. The singular values are equal to the
+        2-norms of the ``n_components`` variables in the lower-dimensional space.
+
+        .. versionadded:: 0.19
+
+    mean_ : array, shape (n_features,)
+        Per-feature empirical mean, estimated from the training set.
+
+        Equal to `X.mean(axis=0)`.
+
+    n_components_ : int
+        The estimated number of components. When n_components is set to 'mle' or a number between 0 and 1 (with
+        svd_solver == 'full') this number is estimated from input data. Otherwise it equals the parameter
+        n_components, or the lesser value of n_features and n_samples if n_components is None.
+
+    n_features_ : int
+        Number of features in the training data.
+
+    n_samples_ : int
+        Number of samples in the training data.
+
+    noise_variance_ : float
+        The estimated noise covariance following the Probabilistic PCA model from Tipping and Bishop 1999. See
+        "Pattern Recognition and Machine Learning" by C. Bishop, 12.2.1 p. 574 or
+        http://www.miketipping.com/papers/met-mppca.pdf. It is required to compute the estimated data covariance and
+        score samples.
+
+        Equal to the average of (min(n_features, n_samples) - n_components) smallest eigenvalues of the covariance
+        matrix of X.
+
+    See Also
+    --------
+    :obj:`sklearn.decomposition.PCA` : Scikit-learn implementation Principal Component Analysis.
+
+    References
+    ----------
+    .. [IS16] Imtiaz, Hafiz, and Anand D. Sarwate. "Symmetric matrix perturbation for differentially-private principal
+        component analysis." In 2016 IEEE International Conference on Acoustics, Speech and Signal Processing (ICASSP),
+        pp. 2339-2343. IEEE, 2016.
     """
     def __init__(self, n_components=None, centered=False, epsilon=1, data_norm=None, range=None, copy=True,
-                 whiten=False, tol=0.0, iterated_power='auto', random_state=None, **unused_args):
-        super().__init__(n_components=n_components, copy=copy, whiten=whiten, svd_solver='full', tol=tol,
-                         iterated_power=iterated_power, random_state=random_state)
+                 whiten=False, random_state=None, **unused_args):
+        super().__init__(n_components=n_components, copy=copy, whiten=whiten, svd_solver='full', tol=0.0,
+                         iterated_power='auto', random_state=random_state)
         self.centered = centered
         self.epsilon = epsilon if centered else epsilon / 2  # Must use some privacy budget for calculating the mean
         self.data_norm = data_norm
