564baf0f4016528867107fc20dbf2a588c4107ff
Naoise Holohan <naoise@nholohan.ie>
2019-12-10 11:08:45 +0000

    Single-source versioning

--- setup.py
+++ setup.py
@@ -1,7 +1,24 @@
+import codecs
+import os
+
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 install_requires = ['numpy >= 1.15.0',
                     'setuptools >= 39.0.1',
@@ -14,9 +31,9 @@ docs_require = ['sphinx >= 1.4',
                 'sphinx_rtd_theme']
 
 setuptools.setup(name='diffprivlib',
-                 version='0.1.2',
+                 version=get_version("diffprivlib/__init__.py"),
                  description='IBM Differential Privacy Library',
-                 long_description=long_description,
+                 long_description=read("README.md"),
                  long_description_content_type='text/markdown',
                  author='Naoise Holohan',
                  author_email='naoise.holohan@ibm.com',
