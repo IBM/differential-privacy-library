382cc3444e54a406417efb14e6c548b5c64e9f30
Naoise Holohan <naoise@nholohan.ie>
2020-05-06 15:30:43 +0100

    Adding Discrete Gaussian mechanism

---
+++ tests/mechanisms/test_GaussianDiscrete.py
@@ -0,0 +1,116 @@
+import numpy as np
+from unittest import TestCase
+
+from diffprivlib.mechanisms import GaussianDiscrete
+from diffprivlib.utils import global_seed
+
+
+class TestGaussianDiscrete(TestCase):
+    def setup_method(self, method):
+        if method.__name__ .endswith("prob"):
+            global_seed(314159)
+
+        self.mech = GaussianDiscrete()
+
+    def teardown_method(self, method):
+        del self.mech
+
+    def test_not_none(self):
+        self.assertIsNotNone(self.mech)
+
+    def test_class(self):
+        from diffprivlib.mechanisms import DPMechanism
+        self.assertTrue(issubclass(GaussianDiscrete, DPMechanism))
+
+    def test_no_params(self):
+        with self.assertRaises(ValueError):
+            self.mech.randomise(1)
+
+    def test_no_epsilon(self):
+        self.mech
+        with self.assertRaises(ValueError):
+            self.mech.randomise(1)
+
+    def test_no_delta(self):
+        self.mech
+        with self.assertRaises(ValueError):
+            self.mech.set_epsilon(0.5)
+
+    def test_large_epsilon(self):
+        self.mech.set_epsilon_delta(1.5, 0.1)
+        self.assertIsNotNone(self.mech.randomise(0))
+
+    def test_complex_epsilon(self):
+        with self.assertRaises(TypeError):
+            self.mech.set_epsilon_delta(1+2j, 0.1)
+
+    def test_string_epsilon(self):
+        with self.assertRaises(TypeError):
+            self.mech.set_epsilon_delta("Two", 0.1)
+
+    def test_non_numeric(self):
+        self.mech.set_epsilon_delta(0.5, 0.1)
+        with self.assertRaises(TypeError):
+            self.mech.randomise("Hello")
+
+    def test_non_int(self):
+        self.mech.set_epsilon_delta(0.5, 0.1)
+        with self.assertRaises(TypeError):
+            self.mech.randomise(1.1)
+
+    def test_simple(self):
+        self.mech.set_epsilon_delta(2, 0.1)
+
+        self.assertIsNotNone(self.mech.randomise(3))
+
+    def test_zero_median_prob(self):
+        self.mech.set_epsilon_delta(0.5, 0.1)
+        vals = []
+
+        for i in range(20000):
+            vals.append(self.mech.randomise(1))
+
+        median = float(np.median(vals))
+        self.assertAlmostEqual(np.abs(median), 1, delta=0.1)
+
+    def test_neighbors_prob(self):
+        epsilon = 1
+        runs = 10000
+        self.mech.set_epsilon_delta(0.5, 0.1)
+        count = [0, 0]
+
+        for i in range(runs):
+            val0 = self.mech.randomise(0)
+            if val0 <= 0.5:
+                count[0] += 1
+
+            val1 = self.mech.randomise(1)
+            if val1 <= 0.5:
+                count[1] += 1
+
+        self.assertGreater(count[0], count[1])
+        self.assertLessEqual(count[0] / runs, np.exp(epsilon) * count[1] / runs + 0.1)
+
+    def test_sanity(self):
+        self.mech.set_epsilon_delta(1, 0.5).check_inputs(1)
+        scale1 = self.mech._scale
+
+        self.mech.set_epsilon_delta(2, 0.75).check_inputs(1)
+        scale2 = self.mech._scale
+
+        self.assertGreater(scale1, scale2)
+
+        self.mech.set_epsilon_delta(3, 4.00902e-10).check_inputs(1)
+        scale3 = self.mech._scale
+
+        self.assertAlmostEqual(2, scale3, places=5)
+
+    def test_bias(self):
+        self.assertEqual(0, self.mech.get_bias(0))
+
+    def test_variance(self):
+        self.assertIsNone(self.mech.get_variance(0))
+
+    def test_repr(self):
+        repr_ = repr(self.mech.set_epsilon_delta(1, 0.5))
+        self.assertIn(".GaussianDiscrete(", repr_)
