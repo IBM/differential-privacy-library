eacaa2ea13c52ece554c31c132c184ce62cdf71c
Naoise Holohan <naoise@nholohan.ie>
2020-10-13 16:29:38 +0100

    Fix Wishart tests for DeprecationWarning

--- tests/mechanisms/test_Wishart.py
+++ tests/mechanisms/test_Wishart.py
@@ -3,6 +3,8 @@ import warnings
 import numpy as np
 from unittest import TestCase
 
+import pytest
+
 from diffprivlib.mechanisms import Wishart
 from diffprivlib.utils import global_seed
 
@@ -28,14 +30,22 @@ class TestWishart(TestCase):
     def generate_data(d=5):
         return np.random.randn(d, d)
 
+    def test_deprecation(self):
+        with warnings.catch_warnings(record=True) as w:
+            warnings.filterwarnings('always', '', DeprecationWarning)
+            self.mech(epsilon=1, sensitivity=1)
+            self.assertIs(w[0].category, DeprecationWarning)
+
     def test_class(self):
         from diffprivlib.mechanisms import DPMechanism
         self.assertTrue(issubclass(Wishart, DPMechanism))
 
+    @pytest.mark.filterwarnings("ignore:The Wishart mechanism has been shown")
     def test_neg_epsilon(self):
         with self.assertRaises(ValueError):
             self.mech(epsilon=-1, sensitivity=1)
 
+    @pytest.mark.filterwarnings("ignore:The Wishart mechanism has been shown")
     def test_inf_epsilon(self):
         mech = self.mech(epsilon=float("inf"), sensitivity=1)
 
@@ -44,6 +54,7 @@ class TestWishart(TestCase):
             noisy_data = mech.randomise(data)
             self.assertTrue(np.allclose(data, noisy_data))
 
+    @pytest.mark.filterwarnings("ignore:The Wishart mechanism has been shown")
     def test_wrong_sensitivity(self):
         with self.assertRaises(TypeError):
             self.mech(epsilon=1, sensitivity="1")
@@ -51,6 +62,7 @@ class TestWishart(TestCase):
         with self.assertRaises(ValueError):
             self.mech(epsilon=1, sensitivity=-1)
 
+    @pytest.mark.filterwarnings("ignore:The Wishart mechanism has been shown")
     def test_zero_sensitivity(self):
         mech = self.mech(epsilon=1, sensitivity=0)
 
@@ -59,59 +71,69 @@ class TestWishart(TestCase):
             noisy_data = mech.randomise(data)
             self.assertTrue(np.allclose(data, noisy_data))
 
+    @pytest.mark.filterwarnings("ignore:The Wishart mechanism has been shown")
     def test_numeric_input(self):
         mech = self.mech(epsilon=1, sensitivity=1)
 
         with self.assertRaises(TypeError):
             mech.randomise(1)
 
+    @pytest.mark.filterwarnings("ignore:The Wishart mechanism has been shown")
     def test_string_input(self):
         mech = self.mech(epsilon=1, sensitivity=1)
 
         with self.assertRaises(TypeError):
             mech.randomise("1")
 
+    @pytest.mark.filterwarnings("ignore:The Wishart mechanism has been shown")
     def test_list_input(self):
         mech = self.mech(epsilon=1, sensitivity=1)
 
         with self.assertRaises(TypeError):
             mech.randomise([1, 2, 3])
 
+    @pytest.mark.filterwarnings("ignore:The Wishart mechanism has been shown")
     def test_string_array_input(self):
         mech = self.mech(epsilon=1, sensitivity=1)
 
         with self.assertRaises(TypeError):
             mech.randomise(np.array([["1", "2"], ["3", "4"]]))
 
+    @pytest.mark.filterwarnings("ignore:The Wishart mechanism has been shown")
     def test_scalar_input(self):
         mech = self.mech(epsilon=1, sensitivity=1)
 
         with self.assertRaises(ValueError):
             mech.randomise(np.array([1]))
 
+    @pytest.mark.filterwarnings("ignore:The Wishart mechanism has been shown")
     def test_scalar_array_input(self):
         mech = self.mech(epsilon=1, sensitivity=1)
 
         self.assertIsNotNone(mech.randomise(np.array([[1]])))
 
+    @pytest.mark.filterwarnings("ignore:The Wishart mechanism has been shown")
     def test_vector_input(self):
         mech = self.mech(epsilon=1, sensitivity=1)
 
         with self.assertRaises(ValueError):
             mech.randomise(np.array([1, 2, 3]))
 
+    @pytest.mark.filterwarnings("ignore:The Wishart mechanism has been shown")
     def test_non_square_input(self):
         mech = self.mech(epsilon=1, sensitivity=1)
 
         with self.assertRaises(ValueError):
             mech.randomise(np.ones((3, 4)))
 
+    @pytest.mark.filterwarnings("ignore:The Wishart mechanism has been shown")
     def test_3D_input(self):
         mech = self.mech(epsilon=1, sensitivity=1)
 
         with self.assertRaises(ValueError):
             mech.randomise(np.ones((3, 3, 3)))
 
+    @pytest.mark.filterwarnings("ignore:The Wishart mechanism has been shown")
     def test_different_result(self):
         mech = self.mech(epsilon=1, sensitivity=1)
         data = self.generate_data()
@@ -123,6 +145,7 @@ class TestWishart(TestCase):
 
             self.assertFalse(np.allclose(noisy_data, old_noisy_data))
 
+    @pytest.mark.filterwarnings("ignore:The Wishart mechanism has been shown")
     def test_repr(self):
         repr_ = repr(self.mech(epsilon=1, sensitivity=1))
         self.assertIn(".Wishart(", repr_)
