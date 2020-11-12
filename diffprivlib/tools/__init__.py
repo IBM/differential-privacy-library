8bf0d46fc36b9400d3e02ce5dbda5e7c84e222f9
Naoise Holohan <naoise@nholohan.ie>
2020-11-12 16:28:23 +0000

    Adding quantile function with docs and tests

--- diffprivlib/tools/__init__.py
+++ diffprivlib/tools/__init__.py
@@ -19,4 +19,5 @@
 Tools for data analysis with differential privacy.
 """
 from diffprivlib.tools.histograms import histogram, histogramdd, histogram2d
+from diffprivlib.tools.quantiles import quantile
 from diffprivlib.tools.utils import count_nonzero, mean, std, sum, var, nanmean, nanstd, nansum, nanvar
