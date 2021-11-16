7d0fb8ce960cb47f516be2a835fe1f24d888021f
Dan Ristea <dan.ristea@protonmail.com>
2021-11-16 09:29:58 +0000

    Use exact equal for bit operations on floats

--- tests/mechanisms/test_Snapping.py
+++ tests/mechanisms/test_Snapping.py
@@ -157,9 +157,9 @@ class TestSnapping(TestCase):
         self.assertEqual(Snapping._get_nearest_power_of_2(0), 0)
 
     def test_get_nearest_power_of_2(self):
-        self.assertAlmostEqual(Snapping._get_nearest_power_of_2(np.nextafter(2.0, math.inf)), 4)
-        self.assertAlmostEqual(Snapping._get_nearest_power_of_2(np.nextafter(2.0, -math.inf)), 2)
-        self.assertAlmostEqual(Snapping._get_nearest_power_of_2(np.nextafter(sys.float_info.min, -math.inf)),
+        self.assertEqual(Snapping._get_nearest_power_of_2(np.nextafter(2.0, math.inf)), 4)
+        self.assertEqual(Snapping._get_nearest_power_of_2(np.nextafter(2.0, -math.inf)), 2)
+        self.assertEqual(Snapping._get_nearest_power_of_2(np.nextafter(sys.float_info.min, -math.inf)),
                                sys.float_info.min)
 
     def test_non_numeric(self):
