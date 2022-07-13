7e8348b7211cb1108c6649df8396a08337b10397
Naoise Holohan <naoise@nholohan.ie>
2022-07-13 13:15:11 +0100

    Snapping docstring nits and typos
    Signed-off-by: Naoise Holohan <naoise@nholohan.ie>

--- diffprivlib/mechanisms/snapping.py
+++ diffprivlib/mechanisms/snapping.py
@@ -17,7 +17,7 @@ class Snapping(LaplaceTruncated):
     r"""
     The Snapping mechanism for differential privacy.
 
-    First proposed by Ilya Mironov [M12]_.
+    First proposed by Ilya Mironov [Mir12]_.
 
     It eliminates a vulnerability stemming from the representation of reals as floating-point numbers in implementations
     of the classic Laplace mechanism and its variants which use the inverse CDF of the Laplace distribution to sample
@@ -28,7 +28,7 @@ class Snapping(LaplaceTruncated):
     Parameters
     ----------
     epsilon : float
-        Privacy parameter :math:`\epsilon` for the mechanism.  Must be in [:math:`2*\eta`, ∞], where math:`\eta` is the
+        Privacy parameter :math:`\epsilon` for the mechanism.  Must be in [:math:`2 \eta`, ∞], where :math:`\eta` is the
         machine epsilon of the floating point type.
 
     sensitivity : float
@@ -39,12 +39,13 @@ class Snapping(LaplaceTruncated):
 
     upper : float
         The upper bound of the mechanism.
+
     References
     ----------
     .. [Mir12] Mironov, Ilya. "On significance of the least significant bits for differential privacy." Proceedings of
      the 2012 ACM conference on Computer and communications security (2012).
-    """
 
+    """
     def __init__(self, *, epsilon, sensitivity, lower, upper):
         super().__init__(epsilon=epsilon, sensitivity=sensitivity, delta=0.0, lower=lower, upper=upper)
         self._bound = self._scale_bound()
@@ -69,6 +70,7 @@ class Snapping(LaplaceTruncated):
         -------
         float
             A symmetric bound around 0 scaled to sensitivity 1
+
         """
         if self.sensitivity == 0:
             return (self.upper - self.lower) / 2.0
@@ -90,14 +92,15 @@ class Snapping(LaplaceTruncated):
 
     def effective_epsilon(self):
         r"""
-        Returns the effective value used in the Snapping mechanism to give the required :math:`(\epsilon, 0)`-DP, based
-        on the bounds and the machine epsilon.
+        Returns the effective value used in the Snapping mechanism to give the required :math:`\epsilon`-DP, based on
+        the bounds and the machine epsilon.
         Based on section 5.2 of [Mir12]_.
 
         Returns
         -------
         float
             The effective value of :math:`\epsilon`
+
         """
         machine_epsilon = np.finfo(float).epsneg
         return (self.epsilon - 2 * machine_epsilon) / (1 + 12 * self._bound * machine_epsilon)
@@ -114,6 +117,7 @@ class Snapping(LaplaceTruncated):
         -------
         float
             value offset to be centered on 0 and scaled to sensitivity 1
+
         """
         value_scaled = value / self.sensitivity
         return value_scaled - self._bound - (self.lower / self.sensitivity)
@@ -179,6 +183,7 @@ class Snapping(LaplaceTruncated):
         ----------
         .. [Py21]  The Python Standard Library. "random — Generate pseudo-random numbers", 2021
         https://docs.python.org/3/library/random.html#recipes
+
         """
         mantissa_size = np.finfo(float).nmant
         mantissa = 1 << mantissa_size | self._rng.getrandbits(mantissa_size)
@@ -201,6 +206,7 @@ class Snapping(LaplaceTruncated):
         -------
         float
             Random value from Laplace distribution scaled according to :math:`\epsilon`
+
         """
         laplace = (-1) ** unif_bit * log_rn(unif)
         return laplace
