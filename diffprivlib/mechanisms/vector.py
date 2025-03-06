Fixing epsilon_p and alpha scaling

diffprivlib/mechanisms/vector.py
@@ -143,13 +143,12 @@ class Vector(DPMechanism):
         """
         self._check_all(value)
 
-        epsilon_p = self.epsilon - 2 * np.log(1 + self.function_sensitivity * self.data_sensitivity /
-                                              self.alpha / self.n)
+        epsilon_p = self.epsilon - 2 * np.log(1 + self.function_sensitivity * self.data_sensitivity / self.alpha)
         delta = 0
 
         if epsilon_p <= 0:
-            delta = (self.function_sensitivity * self.data_sensitivity / (np.exp(self.epsilon / 4) - 1) / self.n
-                     - self.alpha)
+            delta = (self.function_sensitivity * self.data_sensitivity / np.expm1(self.epsilon / 4)
+                     - self.alpha) / self.n
             epsilon_p = self.epsilon / 2
 
         scale = self.data_sensitivity * 2 / epsilon_p

diffprivlib/models/logistic_regression.py
@@ -395,7 +395,7 @@ def _logistic_regression_path(X, y, epsilon, data_norm, pos_class=None, Cs=10, f
     n_iter = np.zeros(len(Cs), dtype=np.int32)
     for i, C in enumerate(Cs):
         l2_reg_strength = 1.0 / (C * n_samples)
-        vector_mech = Vector(epsilon=epsilon, dimension=n_features + int(fit_intercept), alpha=l2_reg_strength,
+        vector_mech = Vector(epsilon=epsilon, dimension=n_features + int(fit_intercept), alpha=1.0 / C,
                              function_sensitivity=0.25, data_sensitivity=data_norm, n=n_samples,
                              random_state=random_state)
         noisy_logistic_loss = vector_mech.randomise(func)